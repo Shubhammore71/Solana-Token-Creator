5509a7e50472d640be01ed313670e55dca7fd3fb
Shubham More <shubham.more@iitgn.ac.in>
2024-03-28 21:28:20 +0530

    inputviewprop3

--- src/views/donate/index.tsx
+++ src/views/donate/index.tsx
@@ -19,10 +19,10 @@ import Branding from "../../components/Branding";
 interface DonateViewProps {
 
   setOpenSendTransaction: (value: boolean) => void;
-  handleFormFieldChange: (fieldName: string, value: any) => void;
+
 }
 
-export const DonateView: FC<DonateViewProps> = ({ setOpenSendTransaction, handleFormFieldChange }) => {
+export const DonateView: FC<DonateViewProps> = ({ setOpenSendTransaction }) => {
   const wallet = useWallet();
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
@@ -136,7 +136,7 @@ export const DonateView: FC<DonateViewProps> = ({ setOpenSendTransaction, handle
                   <InputView
                     name="Amount"
                     placeholder=" amount"
-                    clickhandle={(e) => handleFormFieldChange("amount", e)}
+                    clickhandle={() => {}}
                   />
                 </div>
                 <div className="mt-5 w-full text-center">
