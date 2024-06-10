9a95a7bed2a82fbd8543adda13b35b1568808004
Shubham More <118920195+Shubhammore71@users.noreply.github.com>
2024-06-10 15:43:51 +0530

    Delete node_modules directory

--- node_modules/@solana/wallet-adapter-base/lib/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './adapter.js';
-export * from './errors.js';
-export * from './signer.js';
-export * from './types.js';
-//# sourceMappingURL=index.d.ts.map
--- node_modules/@solana/wallet-adapter-base/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './adapter.js';
-export * from './errors.js';
-export * from './signer.js';
-export * from './types.js';
